1022b2c6060c600fd209022ace114d51277c60f1
vibe008 <104353180+vibe008@users.noreply.github.com>
2022-07-18 19:49:42 +0530

    Updates

---
+++ precache-manifest.a89b3cf430da6747aaa46760b428d9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4674bd71e0cf90b9806b",
+    "url": "/my-app/static/css/main.d3e660f3.chunk.css"
+  },
+  {
+    "revision": "4674bd71e0cf90b9806b",
+    "url": "/my-app/static/js/main.4674bd71.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "fc76a26f182b124c3ee2ba247e957640",
+    "url": "/my-app/index.html"
+  }
+];
